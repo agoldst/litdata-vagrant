194529f0aae872edb9416298ee7ae7bad1c9566a
Dieter Menne <dieter.menne@menne-biomed.de>
2014-05-06 11:57:28 +0200

    Simplified puppet using ->

--- puppet/manifests/rstudio-shiny-server.pp
+++ puppet/manifests/rstudio-shiny-server.pp
@@ -17,64 +17,43 @@ class update_system {
         provider => shell,
         command  => 'apt-get update;',
     }
-
-    package {['software-properties-common',
+    ->
+    package {['software-properties-common','libapparmor1',
               'python-software-properties', 
-              'python', 'g++', 'make','vim', 'whois']:
+              'python', 'g++', 'make','vim', 'whois',
+              'default-jdk', 'gdebi-core', 'libcurl4-gnutls-dev']:
       ensure  => present,
-      require => Exec['apt_update'],
     }
-
-    package{'default-jdk':
-        ensure => present,
-    }
-
-    # Install gdebi
-    package{'gdebi-core':
-        ensure => present,
-    }
-
-    # for RCurl
-    package{'libcurl4-gnutls-dev':
-        ensure => present,
-    }
-
+    ->
     exec {'add-cran-repository':
-      require  => Package['python-software-properties'],
       provider => shell,
       command  =>
       'add-apt-repository "deb http://cran.rstudio.com/bin/linux/ubuntu precise/";
       apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9;
       apt-get update;',
     }
+    -> 
     exec {'upgrade-system':
-      require  => Exec['add-cran-repository'],
       provider => shell,
       command  =>'apt-get -y upgrade',
     }
-
+    ->
     # Install host additions (following https://www.virtualbox.org/manual/ch04.html
     # this must be done after upgrading.
     package {'dkms':
         ensure => present,
-        require =>Exec['upgrade-system'],
     }
 }
 
 # Install r base and packages
 class install_r {
-    package {'r-base':
+    package {['r-base', 'r-base-dev']:
       ensure  => present,
-      require => [Package['dkms','default-jdk','gdebi-core','libcurl4-gnutls-dev']],
-    }
-    package {'r-base-dev':
-      ensure  => present,
-      require => Exec['add-cran-repository'],
-    }
-
+      require => Package['dkms'],
+    }    
+    ->
     exec {'install-r-packages':
         provider => shell,
-        require  => Package['r-base'],
         timeout  => 1200,
         command  => 'Rscript /vagrant/usefulpackages.R'
     }
@@ -91,71 +70,60 @@ class install_shiny_server {
                     'python-software-properties', 'g++']],
         command  => $getshiny
     }
-
+    ->
     # Install shiny server
     exec {'shiny-server-install':
         provider => shell,
-        require  => [Package['gdebi-core'], Exec['shiny-server-download']],
         command  => "gdebi -n ${shinyserver}",
     }
 
-
-    # Copy example shiny files
-    file {'/srv/shiny-server':
-        source  => '/usr/local/lib/R/site-library/shiny/examples',
-        owner   => 'shiny',
-        ensure  => 'directory',
-        require => Exec['shiny-server-install'],
-        recurse => true,
-    }
-
-    # Remove standard app
-    file {'/srv/shiny-server/index.html':
-        require => File['/srv/shiny-server'],
-        ensure => absent,
-    } 
-
+    ->    
     # Create rstudio_users group
     group {'rstudio_users':
         ensure => present,
-        require => File['/srv/shiny-server'],
     }
-
+    ->
   # http://www.pindi.us/blog/getting-started-puppet
     user {'shiny':
         ensure  => present,
-        require => [Exec['shiny-server-install'],File['/srv/shiny-server'],
-                    Group['rstudio_users']],
         groups   => ['rstudio_users'],
         shell   => '/bin/bash',
         managehome => true,
         name    => 'shiny',
         home    => '/srv/shiny-server',
     }   
+    ->
+    # Copy example shiny files
+    file {'/srv/shiny-server':
+        source  => '/usr/local/lib/R/site-library/shiny/examples',
+        owner   => 'shiny',
+        ensure  => 'directory',
+        recurse => true,
+    }   
+    ->
    # Setting password during user creation does not work    
    exec {'shinypassword':
-        require => User['shiny'],
         provider => shell,
         command => 'usermod -p `mkpasswd -H md5 shiny` shiny',
      }
+
+    # Remove standard app
+    file {'/srv/shiny-server/index.html':
+        ensure => absent,
+    } 
 }
 
 # install rstudio and start service
 class install_rstudio_server {
-    package {'libapparmor1':
-        ensure  => present,
-        require => Package['r-base'],
-    }
     # Download rstudio server
     exec {'rstudio-server-download':
+        require  => Package['r-base'],
         provider => shell,
-        require  => Package['libapparmor1'],
         command  => $getrstudio
     }
-
+    ->
     exec {'rstudio-server-install':
         provider => shell,
-        require  => [Package['gdebi-core'], Exec['rstudio-server-download']],
         command  => "gdebi -n ${rstudioserver}",
     }
 }
