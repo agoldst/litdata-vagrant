a885bd87469987253d9e89accfc926ef1889c319
Andrew Goldstone <andrew.goldstone@gmail.com>
2015-01-14 17:58:56 -0500

    install agoldst/litdata on provision

--- r-packages.R
+++ r-packages.R
@@ -2,30 +2,15 @@ options("repos"="http://cran.rstudio.com") # set the cran mirror
 
 packages <- c("devtools",
               "ggplot2",
-#             "reshape2",
-#             "RcppEigen",
+              "reshape2",
+              "dplyr",
               "stringr",
-#             "gridExtra",
-#             "RCurl",
-#             "RJSONIO",
-#             "RJDBC",
-#             "lme4",
-#             "latticeExtra",
-#             "RMySQL",
-#             "XLConnect",
-#             "Cairo",
-             "rstudio",
-             "knitr",
-             "rmarkdown")
+              "rstudio",
+              "knitr",
+              "rmarkdown")
 packages <- setdiff(packages, installed.packages()[, "Package"])
 if (length(packages) != 0){
   (install.packages(packages, dep=c("Depends", "Imports")))
 }
-
-# Packages from github are installed unconditionally
-ghpackages <- c("rstudio/htmltools",
-                "trestletech/shinyTable",
-                "rstudio/shiny")
-# devtools::install_github(ghpackages)
-
+devtools::install_github("agoldst/litdata")
 update.packages(ask=FALSE)
