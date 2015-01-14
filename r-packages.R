61b5939a2dd44b7582573120e8d58308a10bc547
Andrew Goldstone <andrew.goldstone@gmail.com>
2015-01-14 14:21:42 -0500

    a few more R packages allowed

--- r-packages.R
+++ r-packages.R
@@ -1,22 +1,22 @@
 options("repos"="http://cran.rstudio.com") # set the cran mirror
 
 packages <- c("devtools",
-#             "ggplot2",
-#             "plyr",
+              "ggplot2",
 #             "reshape2",
 #             "RcppEigen",
-#             "stringr",
+              "stringr",
 #             "gridExtra",
 #             "RCurl",
 #             "RJSONIO",
 #             "RJDBC",
-#             "knitr",
 #             "lme4",
 #             "latticeExtra",
 #             "RMySQL",
 #             "XLConnect",
 #             "Cairo",
-             "rstudio")
+             "rstudio",
+             "knitr",
+             "rmarkdown")
 packages <- setdiff(packages, installed.packages()[, "Package"])
 if (length(packages) != 0){
   (install.packages(packages, dep=c("Depends", "Imports")))
@@ -25,7 +25,6 @@ if (length(packages) != 0){
 # Packages from github are installed unconditionally
 ghpackages <- c("rstudio/htmltools",
                 "trestletech/shinyTable",
-                "rstudio/rmarkdown",
                 "rstudio/shiny")
 # devtools::install_github(ghpackages)
 
